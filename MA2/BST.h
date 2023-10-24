1910724052ba9e3c5e54ef059c8022ed76c17568
shivtriv1 <131293607+shivtriv1@users.noreply.github.com>
2023-10-24 11:13:57 -0700

    Add files via upload

---
+++ MA2/BST.h
@@ -0,0 +1,344 @@
+#ifndef __BST_H
+#define __BST_H
+
+#include <iostream>
+#include <limits>
+using namespace std;
+
+
+/*
+ *  Data structure for a single tree node
+ */
+template <typename T>
+struct Node {
+public:
+	T value;
+	Node* left;
+	Node* right;
+
+	Node(T val) {
+		this->value = val;
+		this->left = nullptr;
+		this->right = nullptr;
+	}
+
+	~Node()
+	{
+		this->value = 0;
+		this->left = nullptr;
+		this->right = nullptr;
+	}
+};
+
+/*
+ * Binary Search Tree (BST) class implementation
+ */
+template <typename T>
+class BST {
+
+protected:
+	Node<T>* _root;         // Root of the tree nodes
+
+	/* Add new T val to the tree */
+	void addHelper(Node<T>* root, T val) {
+		if (root->value > val) {
+			if (!root->left) {
+				root->left = new Node<T>(val);
+			}
+			else {
+				addHelper(root->left, val);
+			}
+		}
+		else {
+			if (!root->right) {
+				root->right = new Node<T>(val);
+			}
+			else {
+				addHelper(root->right, val);
+			}
+		}
+	}
+
+	/* Print tree out in inorder (A + B) */
+	void printInOrderHelper(Node<T>* root) {
+		if (!root) return;
+		printInOrderHelper(root->left);
+		cout << root->value << ' ';
+		printInOrderHelper(root->right);
+	}
+
+	/* Return number of nodes in tree */
+	int nodesCountHelper(Node<T>* root) {
+		if (!root) {
+			return 0;
+		}
+		else {
+			return 1 + nodesCountHelper(root->left) + nodesCountHelper(root->right);
+		}
+	}
+
+	/* Return height of tree (root == nullptr -> 0) */
+	int heightHelper(Node<T>* root) {
+		if (!root) {
+			return -1;
+		}
+		else {
+			return 1 + max(heightHelper(root->left), heightHelper(root->right));
+		}
+	}
+
+	/* Delete a given <T> value from tree */
+	bool deleteValueHelper(Node<T>* parent, Node<T>* current, T value) {
+		if (!current) return false;
+		if (current->value == value) {
+			if (current->left == nullptr || current->right == nullptr) {
+				Node<T>* temp = current->left;
+				if (current->right) temp = current->right;
+				if (parent) {
+					if (parent->left == current) {
+						parent->left = temp;
+					}
+					else {
+						parent->right = temp;
+					}
+				}
+				else {
+					this->_root = temp;
+				}
+			}
+			else {
+				Node<T>* validSubs = current->right;
+				while (validSubs->left) {
+					validSubs = validSubs->left;
+				}
+				T temp = current->value;
+				current->value = validSubs->value;
+				validSubs->value = temp;
+				return deleteValueHelper(current, current->right, temp);
+			}
+			delete current;
+			return true;
+		}
+		return deleteValueHelper(current, current->left, value) ||
+			deleteValueHelper(current, current->right, value);
+	}
+
+	/********************************* PUBLIC API *****************************/
+public:
+
+	BST() : _root(nullptr) { }               // Basic initialization constructor
+
+	/**
+	 * Destructor - Needs to free *all* nodes in the tree
+	 * TODO: Implement Destructor
+	 */
+	~BST() {
+	   
+		// cout << "TODO: Implement Destructor" << endl;
+		deletetree(_root);
+	}
+
+	void deletetree(Node<T>* root)
+	{
+		if (_root == nullptr) 
+		{
+			cout << "empty" << endl;
+			return;
+		}
+
+		deletetree(root->left);
+		deletetree(root->right);
+
+		delete root;
+	}
+
+	/* Public API */
+	void add(T val) {
+		if (this->_root) {
+			this->addHelper(this->_root, val);
+		}
+		else {
+			this->_root = new Node<T>(val);
+		}
+	}
+
+	void print() {
+		printInOrderHelper(this->_root);
+	}
+
+	/**
+	 * Print the nodes level by level, starting from the root
+	 * TODO: Implement printLevelOrder
+	 */
+	
+	void printLevelOrder() {
+		//cout << "TODO: Implement printLevelOrder" << endl;
+		
+		//Node<T>* empT = nullptr;
+
+		
+		int levelOrders = 0;
+		int heightCount = height();
+		
+		//for (int i = 0; i < heightCount; levelOrders++) {}
+		
+		cout << heightCount << endl;
+		
+
+
+	}
+
+	int nodesCount() {
+		return nodesCountHelper(this->_root);
+	}
+
+	int height() {
+		return heightHelper(this->_root);
+	}
+
+	/**
+	 * Print the maximum path in this tree
+	 * TODO: Implement printMaxPath
+	 */
+	void printMaxPath() {
+		//cout << "TODO: Implement printMaxPath" << endl;
+
+		T temp, temp2;
+
+		if (this->_root != nullptr)
+		{
+			temp = this->_root->value;
+			cout << temp << " ";
+		}
+
+		if (this->_root->left != nullptr)
+		{
+
+			temp = this->_root->left->value;
+
+			cout << temp << " ";
+
+
+
+			if (this->_root->right != nullptr)
+			{
+				temp2 = this->_root->right->value;
+
+				cout << temp2 << " ";
+
+
+
+			}
+
+		}
+
+		if (this->_root->right != nullptr)
+		{
+
+			temp = this->_root->value;
+
+			cout << temp << " ";
+
+			temp = this->_root->right->value;
+
+			if (this->_root->left != nullptr)
+			{
+				temp2 = this->_root->left->value;
+
+
+				cout << temp << " ";
+
+				
+			}
+		}
+		
+	}
+
+	bool deleteValue(T value) {
+		return this->deleteValueHelper(nullptr, this->_root, value);
+	}
+
+	/**
+	 * Find if the BST contains the value
+	 * TODO: Implement contains
+	 */
+	bool contains(T value) {
+		T temp, temp2;
+
+		if (this->_root != nullptr)
+		{
+			temp = this->_root->value;
+
+			if (temp == value)
+			{
+				return true;
+			}
+		}
+
+			if (this->_root->left != nullptr)
+			{
+
+				temp = this->_root->left->value;
+
+
+				if (temp == value)
+				{
+					return true;
+				}
+				else
+				{
+					temp = this->_root->left->value;
+				}
+
+
+				if (this->_root->right != nullptr)
+				{
+					temp2 = this->_root->right->value;
+
+					if (temp2 == value || temp == value)
+					{
+						return 1;
+					}
+
+
+				}
+
+			}
+		
+				if(this->_root->right != nullptr)
+				{
+					
+					temp = this->_root->value;
+
+
+					if (temp == value)
+					{
+						return true;
+					}
+					else
+					{
+						temp = this->_root->right->value;
+					}
+
+					if (this->_root->left != nullptr)
+					{
+						temp2 = this->_root->left->value;
+
+						
+						if (temp2 == value || temp == value)
+						{
+							return 1;
+						}
+					}
+				}
+			
+		
+		return 0;
+		//return numeric_limits<T>::min();
+
+
+	}
+};
+
+#endif
+
